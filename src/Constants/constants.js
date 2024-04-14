e47896dc3af63a090539213ae6f1ffbafc34fb99
karim mattar <karimhady111@gmail.com>
2024-04-14 07:52:34 +0200

    first commit

---
+++ src/Constants/constants.js
@@ -0,0 +1,220 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: constants.js
+   Version: I
+   Creation: 02/06/2023
+   Last modification: 18/12/2023
+*/
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faX,
+  faBars,
+  faWindowRestore,
+  faBagShopping,
+  faDiceD6,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faReact,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+
+import {
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  awsIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  psqlIcon,
+  eslintIcon,
+  pyIcon,
+  viteIcon,
+  nodeIcon,
+  raspIcon,
+  neoIcon,
+  figmaIcon,
+  xpovi,
+  knowvte,
+  femto,
+  avatar,
+  lms,
+} from '../assets';
+
+library.add(faX, faBars, faWindowRestore, faBagShopping, faDiceD6);
+
+const media = {
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  awsIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  psqlIcon,
+  eslintIcon,
+  pyIcon,
+  viteIcon,
+  nodeIcon,
+  raspIcon,
+  neoIcon,
+  figmaIcon,
+  avatar,
+};
+
+const icons = {
+  faBars,
+  faX,
+  faWindowRestore,
+  faBagShopping,
+  faDiceD6,
+  faReact,
+  faGithub,
+  faLinkedin,
+  faEnvelope,
+};
+
+const introduction = {
+  text: [
+    "Hello there, I'm so happy you are here! (:",
+		"My name is Karim, I'm 28 and a senior back-end engineer.",
+    "A Talented Python full stack developer with over 4 years of experience in web development.",
+    "Specialized in creating customized solutions to meet specific needs.",
+    "Eager to take on a challenging role that offers advanced opportunities to leverage my expertise and skills to drive company success.",
+    "I'm continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !",
+  ],
+};
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'skills',
+    title: 'Skills',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const projects = [
+  {
+    name: "FemtoFP&A",
+    description: 'AI powered Financial Planning & Analysis SaaS; Comprehensive analytics, performance monitoring & insightful reportingTo level up planning & decision making in real-time.',
+    image: femto,
+    source_code_link: 'https://github.com/femtofpa',
+    demo_link: 'https://www.femtofpa.com',
+  },
+  {
+    name: 'Xpovi',
+    description: 'An enterprise solution enabling startups, to tailor and receive their financial models and business plans in few clicks.',
+    image: xpovi,
+    source_code_link: 'https://www.xpovi.com',
+    demo_link: 'https://www.xpovi.com',
+  },
+  {
+    name: 'KnowVTE',
+    description: 'VTE is the third most common cause of cardiovascular death, following acute coronary artery disease and stroke, and is responsible for more than 3 million deaths per year worldwide.',
+    image: knowvte,
+    source_code_link: 'https://github.com/Conference-Organizing-Bureau-COB/gahar',
+    demo_link: 'https://knowvte.com',
+  },
+];
+
+const memoji = {
+  image: [avatar],
+};
+
+const skills = [
+  {
+    id: 'html',
+    title: 'HTML',
+    icon: htmlIcon,
+    description:
+      'I have a strong command of HTML for organizing web pages and generating meaningful content that can be accessed by all users.',
+  },
+  {
+    id: 'css',
+    title: 'CSS',
+    icon: cssIcon,
+    description:
+    'I possess expertise in utilizing CSS to design web pages and craft visually captivating layouts that enhance the overall user experience.',
+  },
+  {
+    id: 'javascript',
+    title: 'JavaScript',
+    icon: jsIcon,
+    description:
+    'I have substantial experience in employing JavaScript to introduce interactivity and functionality into web pages, resulting in dynamic user interfaces.',
+  },
+  {
+    id: 'react',
+    title: 'React',
+    icon: reactIcon,
+    description:
+      'I am well-versed in React, proficient in creating reusable components and managing application state using hooks and context.',
+  },
+  {
+    id: 'aws',
+    title: 'Amazon Web Services',
+    icon: awsIcon,
+    description:
+      'I am certified in AWS and proficient in working with EC2 and RDS instances, leveraging the power of cloud computing for scalable and reliable infrastructure.',
+  },
+  {
+    id: 'git',
+    title: 'Git',
+    icon: gitIcon,
+    description:
+    'I am proficient in Git, managing code changes, collaborating with others, and resolving conflicts effectively.',
+  },
+  {
+    id: 'github',
+    title: 'GitHub',
+    icon: githubIcon,
+    description:
+      'I am skilled in using GitHub for seamless project collaboration, code sharing, and issue tracking. Through GitHub, I efficiently create and manage repositories and effectively present my work to potential employers.',
+  },
+  {
+    id: 'psql',
+    title: 'Postgresql',
+    icon: psqlIcon,
+    description:
+      'I have a strong command of PostgreSQL, encompassing a wide range of skills such as database normalization, triggers, front-end connectivity, and data analysis using software like Power BI.',
+  },
+  {
+    id: 'py',
+    title: 'Python',
+    icon: pyIcon,
+    description:
+      'With 3 years of Python experience, I am adept at coding functions and creating graphic interfaces using Tkinter.',
+  },
+];
+
+const markerSvg = `<svg viewBox="-4 0 36 36">
+    <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
+    <circle fill="black" cx="14" cy="14" r="7"></circle>
+  </svg>`;
+
+export {
+  media,
+  introduction,
+  projects,
+  memoji,
+  skills,
+  markerSvg,
+  icons,
+};
---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: index.js
+   Version: I
+   Creation: 02/06/2023
+   Last modification: 03/06/2023
+*/
+
+// Logo
+import karim from './logo/karim.svg';
+
+// Technologies
+import htmlIcon from './tech/html.png';
+import cssIcon from './tech/css.png';
+import jsIcon from './tech/javascript.png';
+import reactIcon from './tech/reactjs.png';
+import awsIcon from './tech/aws.png';
+import javaIcon from './tech/java.png';
+import gitIcon from './tech/git.png';
+import githubIcon from './tech/github.png';
+import figmaIcon from './tech/figma.png';
+import psqlIcon from './tech/postgresql.png';
+import eslintIcon from './tech/eslint.png';
+import viteIcon from './tech/vite.svg';
+import pyIcon from './tech/py.png';
+import nodeIcon from './tech/node.webp';
+import neoIcon from './tech/neo.png';
+import raspIcon from './tech/raspi.png';
+import close from './close.svg';
+import menu from './menu.svg';
+
+// Projects
+import xpovi from './projects/xpovi.jpeg';
+import knowvte from './projects/knowvte.png';
+import femto from './projects/femto.png';
+import lms from './projects/lms.png'
+
+// Avatar
+import avatar from './avatar/avatar.png';
+
+export {
+  menu,
+  close,
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  awsIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  eslintIcon,
+  figmaIcon,
+  psqlIcon,
+  nodeIcon,
+  pyIcon,
+  viteIcon,
+  neoIcon,
+  raspIcon,
+  xpovi,
+  knowvte,
+  femto,
+  avatar,
+  karim,
+  lms
+};
